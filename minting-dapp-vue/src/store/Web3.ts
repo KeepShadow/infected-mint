dc6e52b381bd637db5234a5fac5fa6180f0eabbb
Vanojx1 <nicolavanoni.dev@gmail.com>
2023-07-10 17:38:11 +0200

    Sepolia

--- minting-dapp-vue/src/store/Web3.ts
+++ minting-dapp-vue/src/store/Web3.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 import { EthereumClient, w3mConnectors } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/html'
 import { configureChains, createConfig, getContract, prepareWriteContract, writeContract, waitForTransaction } from '@wagmi/core'
-import { polygon } from '@wagmi/core/chains'
+import { mainnet, sepolia } from '@wagmi/core/chains'
 import { publicProvider } from '@wagmi/core/providers/public'
 import { toast } from 'vue3-toastify'
 
@@ -57,7 +57,7 @@ const defaultState: State = {
 const projectId = CollectionConfig.walletConnectProjectId as string
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [polygon],
+  [mainnet, sepolia],
   [publicProvider()]
 )
 
